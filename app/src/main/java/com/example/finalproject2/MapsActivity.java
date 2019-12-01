8f3f415d500563f8bc5c282617d5722100a3537e
bojiang3 <bojiang3@illinois.edu>
2019-12-01 01:09:00 -0600

    "infowindow demo finished, button in progress"

---
+++ app/src/main/java/com/example/finalproject2/MapsActivity.java
@@ -0,0 +1,166 @@
+package com.example.finalproject2;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+//Jenny 1127 22:10 start:
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+//Jenny 1127 22:10 end.
+
+//Jenny 1128 start:
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+//Jenny 1128 end.
+
+//1130 21:00 start:
+import android.util.Pair;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+
+import org.w3c.dom.Text;
+//1130 21:00 end.
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap map;
+
+    //Jenny 1127 22:10 start:
+    /** Create a MarkerOptions object to specify where we want the marker. */
+    private MarkerOptions options;
+
+    /** Add it to the map - Google Maps gives us the created Marker. */
+    private Marker marker;
+
+    /** The list of Markers that mark the printers. */
+    private List<Marker> markers = new ArrayList();
+    //Jenny 1127 22:10 end.
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        //Jenny 1127 22:00 start:
+        placeMarker();
+        //Jenny 1127 22:00 end.
+
+        //Jenny 1127 16:00 start:
+        // Add a marker to each of the printers on UIUC campus.
+        // "ACES AESB"
+        LatLng ACES_AESB = new LatLng(40.103612, -88.226026);
+        map.moveCamera(CameraUpdateFactory.newLatLng(ACES_AESB));
+        //Jenny 1127 16:00 end.
+
+        //Bojiang 11/30 start:
+        // Reference: Stackoverflow
+        // https://stackoverflow.com/questions/13904651/android-google-maps-v2-how-to-add-marker-with-multiline-snippet
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                LinearLayout info = new LinearLayout(getApplicationContext());
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(getApplicationContext());
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(getApplicationContext());
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
+        //Bojiang 11/30 ends. Multi-snippet infowindow resolved.
+
+    }
+
+
+
+    //Jenny 1127 22:00 start:
+    public void placeMarker() { // Suppose position is a LatLng variable
+
+        //Information about the list of printers is added to be stored in "printerLocationMap"
+        Printers.addPrintersToList();
+
+        for (Map.Entry<String, Pair<LatLng, String>> entry : Printers.printerLocationMap.entrySet()) {
+            //iterate through each printer:
+
+            String printerLocationName = entry.getKey(); //name of the building where the printer is
+            LatLng printerLocation = entry.getValue().first; //location of the printer
+            String printerInfo = entry.getValue().second;
+
+            //Add a marker to the location of each printer, with title being the building name.
+            options = new MarkerOptions().position(printerLocation)
+                    .title(printerLocationName)
+                    .snippet(printerInfo);
+            marker = map.addMarker(options);
+
+            //Set the default color of the marker to be blue.
+            BitmapDescriptor defaultColor = BitmapDescriptorFactory.defaultMarker(Printers.defaultMarkerColor);
+            marker.setIcon(defaultColor);
+
+            //Add each marker to "markers", the list of markers.
+            markers.add(marker);
+            marker.showInfoWindow();
+        }
+    }
+
+    //Jenny 1127 22:00 end.
+
+
+}
---
+++ app/src/main/java/com/example/finalproject2/Printers.java
@@ -0,0 +1,95 @@
+package com.example.finalproject2;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+//Jenny 1127 start:
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.GoogleMap;
+import java.util.Map;
+import java.util.HashMap;
+import com.google.android.gms.maps.model.LatLngBounds;
+//Jenny 1127 end.
+
+//1130;
+import android.util.Pair;
+//1130.
+
+public class Printers {
+
+    /**
+     * Store all the printers' locations(LatLng) and Marker Options.
+     * Example1: EH = the LatLng variable (location) of Engineering Hall; EHMarker = the Marker Option variable of Engineering Hall.
+     * Example2: IlliniUnion = the LatLng variable (location) of Illini Union; IlliniUnionMarker = the Marker Option variable of Illini Union.
+     * Finished by Bojiang Li on 11/27/2019.
+     *
+     */
+
+    static final Map<String, Pair<LatLng, String>> printerLocationMap = new HashMap<>();
+
+
+    static final LatLngBounds campusUIUC = new LatLngBounds(new LatLng(38.00, -80), new LatLng(42, -85));
+
+    static final float defaultMarkerColor = BitmapDescriptorFactory.HUE_BLUE;
+
+    static final float clickedMarkerColor = BitmapDescriptorFactory.HUE_RED;
+
+
+    public static void addPrintersToList() {
+
+
+
+        Pair<LatLng, String> ACES = new Pair<>(new LatLng(40.103039, -88.225101),
+                " aces-print\\abe-220hp3015\n" +
+                "ACES AESB - Rm 220\n" +
+                " aces-print\\ACF-BW (virtual)\n" +
+                "ACES Library - Lower Level - Academic Computing Facility\n" +
+                " aces-print\\ACF-COLOR (virtual)\n" +
+                "ACES Library - Lower Level - Academic Computing Facility\n" +
+                "<a href=\"http://abcd.efg.com\" target=\"_blank\">http://abcd.efg.com</a>");
+        printerLocationMap.put("ACES Library", ACES);
+
+        Pair<LatLng, String> EB = new Pair<>(new LatLng(40.102113, -88.229611), "\n" +
+                " ed-file\\ED-25-M506\n" +
+                "Black and White | Education Building Room 25\n" +
+                " ed-file\\ED-25-S5840\n" +
+                "ED 25\n");
+        printerLocationMap.put("Education Building", EB);
+
+        Pair<LatLng, String> ECEB = new Pair<>(new LatLng(40.115144, -88.228017), "\n" +
+                " engr-print-01\\eceb-C1060-printer\n" +
+                " engr-print-02\\eceb-1001-bwd1\n" +
+                " engr-print-02\\eceb-1016-bwd\n" +
+                " engr-print-02\\eceb-2026-bwd1\n" +
+                " engr-print-02\\eceb-2026-color1\n" +
+                " engr-print-02\\eceb-2070-bwd1\n" +
+                " engr-print-02\\eceb-2076-bwd1\n" +
+                " engr-print-02\\eceb-3016-bwd1\n" +
+                " engr-print-02\\eceb-3022-bwd1\n" +
+                " engr-print-02\\eceb-3070-bwd1\n" +
+                " engr-print-02\\eceb-3070-color1\n" +
+                " engr-print-02\\eceb-4072-bwd1\n" +
+                " engr-print-02\\eceb-4074-bwd1\n" +
+                " engr-print-02\\eceb-5072-bwd\n" +
+                " engr-print-02\\eceb-5074-bwd1\n" +
+                " engr-print-02\\eceb-5076-bwd1\n");
+        printerLocationMap.put("Electrical and Computer Engineering Building", ECEB);
+
+        Pair<LatLng, String> EH = new Pair<>(new LatLng(40.111036, -88.226990), "\n" +
+                " engr-print-02\\eh-406b1-color1\n" +
+                " engr-print-02\\eh-406b1-pool (virtual)\n" +
+                " engr-print-02\\eh-406b8-color1\n" +
+                " engr-print-02\\eh-406b8-pool (virtual)\n");
+        printerLocationMap.put("Engineering Hall", EH);
+
+        Pair<LatLng, String> Everitt = new Pair<>(new LatLng(40.111165, -88.228348), "" +
+                "\n" +
+                " engr-print-01\\everitt-1126-printer\n" +
+                " engr-print-01\\everitt-2201-bw\n" +
+                " engr-print-01\\everitt-2240-color\n" +
+                " engr-print-01\\everitt-receiving-copier(Everitt 0321)\n");
+        printerLocationMap.put("Everitt Laboratory", Everitt);
+    }
+}
